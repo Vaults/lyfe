485cfb9738b764b8cc6aab7f1ee55c44583444c4
Marcel Visser <marcel1337@hotmail.com>
2017-01-17 18:22:48 +0100

    Style & datastructure update

--- js/main.js
+++ js/main.js
@@ -1,8 +1,34 @@
-angular.module('life').controller('testCtrl', function($scope){
-	var s = 40;
-	var alpha = ['a','b','c']
-	var random=function(o){return alpha[~~(alpha.length*Math.random())]}
+var pickRandom = (a) => a[~~(a.length * Math.random())];
+
+angular.module('life', []).controller('testCtrl', function($scope){
+	var s = 69;
+    var critterChance = 0.1;
+
+    var tile = (c,cl, x, y) => ({
+        char: c, col: cl, x: x, y: y,
+    });
+    var critter = (c,cl,x,y)=> $.extend(tile(c,cl), {});
+	var alpha = [
+        {char:'x',col:'red'},
+        {char:'o',col:'blue'},
+        {char:'a',col:'purple'}];
+
+    var randomTile = (x, y) => {
+        if(Math.random() > 0.1){
+            return tile(' ', ' ', x, y);
+        }
+        var a = pickRandom(alpha);
+        return critter(a.char, a.col, x, y);
+    };
+
 	
-	$scope.field = new Array(s).fill('').map(o=>new Array(s).fill(' ').map(random));
+	$scope.field = new Array(s).fill('').map(
+        (o,x) => new Array(s).fill('').map(
+            (o,y) =>  randomTile(x, y)
+        )
+    );
+
+
+
 	console.log($scope.field);
 })
