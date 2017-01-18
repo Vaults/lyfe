a0d9edfb6b5a114aa9291dfe2c487b391023422e
Marcel Visser <marcel1337@hotmail.com>
2017-01-18 01:16:53 +0100

    Critters running around aimlessly

---
+++ js/lib.js
@@ -0,0 +1,5 @@
+var pickRandom = (a) => a[~~(Math.random() * a.length)];
+var randNeg = (i) => (rand(1) == 1) ? i : -i;
+var rand = (a, b) => (b) ? (a + Math.round(Math.random() * b)) : Math.round(Math.random() * (a));
+var clamp = (a, x, b) => (x < a) ? a : ((x > b) ? b : x);
+var eqCoords = (a, b) => a.x == b.x && a.y == b.y;
--- js/main.js
+++ js/main.js
@@ -1,34 +1,89 @@
-var pickRandom = (a) => a[~~(a.length * Math.random())];
+angular.module('life', []).controller('testCtrl', function ($scope, $timeout) {
+    var s = 20;
+    var maxCritterCount = 50;
+    ;
 
-angular.module('life', []).controller('testCtrl', function($scope){
-	var s = 69;
-    var critterChance = 0.1;
-
-    var tile = (c,cl, x, y) => ({
+    var tile = (c, cl, x, y) => ({
         char: c, col: cl, x: x, y: y,
     });
-    var critter = (c,cl,x,y)=> $.extend(tile(c,cl), {});
-	var alpha = [
-        {char:'x',col:'red'},
-        {char:'o',col:'blue'},
-        {char:'a',col:'purple'}];
-
-    var randomTile = (x, y) => {
-        if(Math.random() > 0.1){
-            return tile(' ', ' ', x, y);
+    var critter = (c, cl, x, y)=> $.extend(tile(c, cl, x, y), {
+        move: function () {
+            return {
+                'from': this,
+                'to': {x: clamp(0, this.x + randNeg(rand(1)), s - 1), y: clamp(0, this.y + randNeg(rand(1)), s - 1)}
+            };
         }
+    });
+    var emptyTile = (x, y) => tile(' ', 'light-green', x, y);
+    var alpha = [
+        {char: 'x', col: 'red'},
+        {char: 'o', col: 'blue'},
+        {char: 'a', col: 'purple'}];
+
+
+    var objs = new Array(s).fill('').map(o => ({}));
+
+    for (var i = 0; i < maxCritterCount; i++) {
         var a = pickRandom(alpha);
-        return critter(a.char, a.col, x, y);
-    };
+        var x = rand(s - 1);
+        var y = rand(s - 1);
+        objs[x][y] = critter(a.char, a.col, x, y);
+    }
 
-	
-	$scope.field = new Array(s).fill('').map(
-        (o,x) => new Array(s).fill('').map(
-            (o,y) =>  randomTile(x, y)
-        )
-    );
+    var render = () => {
+        $scope.field = new Array(s).fill('').map(
+            (o, x) => new Array(s).fill('').map(
+                (o, y) => {
+                    return (objs[x][y]) ? objs[x][y] : emptyTile(x, y)
+                }
+            )
+        );
+    }
 
+    var doMoves = () => {
+        var moves = [];
+        objs.forEach(y => {
+            for (key in y) {
+                o = y[key];
+                if (o) {
+                    if (o.move) {
+                        moves.push(o.move());
+                    }
+                }
+            }
+            ;
+        });
+        moves.forEach(o => {
+            console.log(o.to.x);
+            if (!eqCoords(o.to, o.from) && !objs[o.to.x][o.to.y]) {
+                objs[o.to.x][o.to.y] = o.from;
+                objs[o.from.x][o.from.y] = null;
+                o.from.x = o.to.x;
+                o.from.y = o.to.y;
+            }
+        });
+    }
 
+    var engineLoop = () => {
+        doMoves();
 
-	console.log($scope.field);
+        render();
+        $timeout(engineLoop, 500);
+    }
+    engineLoop();
+
 })
+
+
+/* var randomTile = (x, y) => {
+ var x = Math.random();
+ if(x > 0.3){
+ return emptyTile(x,y);
+ }else if(x > 0.1){
+ return tile(' ', 'green', x, y);
+ }else if(x > 0.05){
+ return tile(' ', 'light-blue', x, y);
+ }
+ var a = pickRandom(alpha);
+ return critter(a.char, a.col, x, y);
+ };*/
