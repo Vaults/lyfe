e0e7c0046aed09e5a95fb10266351af13aca76a2
Marcel Visser <marcel1337@hotmail.com>
2017-01-18 18:16:15 +0100

    Simple sequencing for critters

--- js/main.js
+++ js/main.js
@@ -1,25 +1,117 @@
 angular.module('life', []).controller('testCtrl', function ($scope, $timeout) {
-    var s = 15;
+    var s = 25;
     var maxCritterCount = 15;
     var moveChance = 0.10;
+    var FPS = 60;
 
     var tile = (c, cl, x, y) => ({
         char: c, col: cl, x: x, y: y,
     });
     var critter = (c, cl, x, y)=> $.extend(tile(c, cl, x, y), {
-        move: function () {
+        moveRandom: function () {
             var xfac = 0
             var yfac = 0;
-            if(rand(1/moveChance) < 1) {
+            if (rand(1 / moveChance) < 1) {
                 if (rand(1) > 0) {
                     xfac = randNeg(rand(1));
                 } else {
                     yfac = randNeg(rand(1));
                 }
             }
+            return {x: xfac, y: yfac};
+        },
+        sequences: {
+            currSequence: {
+                state: false,
+                sequence: '',
+                phase: 0,
+                maxPhase: -1
+            },
+            inSequence: function(){
+                return this.currSequence.state;
+            },
+            startSequence: function(f){
+                var p = this[f].maxPhase;
+                this.currSequence = {
+                    state: true,
+                    sequence: f,
+                    phase: 0,
+                    maxPhase: p
+                }
+            },
+            endSequence: function() {
+                this.currSequence = {
+                    state: false,
+                    sequence: '',
+                    phase: 0,
+                    maxPhase: -1
+                }
+            },
+            incPhase: function() {
+                this.currSequence.phase++;
+            },
+            isDone: function() {
+                return this.currSequence.phase == this.currSequence.maxPhase;
+            },
+            run: function() {
+                var ret = this[this.currSequence.sequence].f(this.currSequence.phase);
+                this.incPhase();
+                if (this.isDone()) {
+                    this.endSequence();
+                }
+                return ret;
+            },
+
+            moveBlock: {
+                'f': (p) => {
+                    var dirs = {
+                        "0": {x: 1, y: 0},
+                        "1": {x: 1, y: 0},
+                        "2": {x: 0, y: 1},
+                        "3": {x: 0, y: 1},
+                        "4": {x: -1, y: 0},
+                        "5": {x: -1, y: 0},
+                        "6": {x: 0, y: -1},
+                        "7": {x: 0, y: -1},
+                    }
+                    return dirs[p];
+                }, maxPhase: 7
+            },
+            moveLine: {
+                'f': (p) => {
+                    var dirs = {
+                        "0": {x: 1, y: 0},
+                        "1": {x: 1, y: 0},
+                        "2": {x: 1, y: 0},
+                        "3": {x: 1, y: 0},
+                        "4": {x: -1, y: 0},
+                        "5": {x: -1, y: 0},
+                        "6": {x: -1, y: 0},
+                        "7": {x: -1, y: 0},
+                    }
+                    return dirs[p];
+                }, maxPhase: 7
+            },
+        },
+
+
+        move: function () {
+            var mod = {x: 0, y: 0};
+            if (!this.sequences.inSequence()) {
+                if(rand(1000) < 1) {
+                    this.sequences.startSequence('moveBlock');
+                }else if(rand(1000) < 2) {
+                    this.sequences.startSequence('moveLine');
+                }else{
+                    mod = this.moveRandom();
+                }
+            }else {
+                mod = this.sequences.run();
+            }
+
             return {
                 'from': this,
-                'to': {x: clamp(0, this.x + xfac, s - 1), y: clamp(0, this.y + yfac, s - 1)}
+                'to': {x: clamp(0, this.x + mod.x, s - 1), y: clamp(0, this.y + mod.y, s - 1)}
             };
         }
     });
@@ -30,7 +122,7 @@ angular.module('life', []).controller('testCtrl', function ($scope, $timeout) {
         {char: 'a', col: 'purple'}];
 
     var objs = '';
-    var initEngine = ()=>{
+    var initEngine = ()=> {
         objs = new Array(s).fill('').map(o => ({}));
 
         for (var i = 0; i < maxCritterCount; i++) {
@@ -47,7 +139,6 @@ angular.module('life', []).controller('testCtrl', function ($scope, $timeout) {
             )
         );
     }
-
 
 
     var render = () => {
@@ -83,9 +174,8 @@ angular.module('life', []).controller('testCtrl', function ($scope, $timeout) {
         doMoves();
 
         render();
-        $timeout(engineLoop, 1000/60);
+        $timeout(engineLoop, 1000 / FPS);
     }
-
 
 
     initEngine();
