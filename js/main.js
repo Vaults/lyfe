cd2b8fc043774daa8bb9b0578958050b237e0b0d
Marcel Visser <marcel1337@hotmail.com>
2017-01-18 12:53:10 +0100

    Efficiency & stability

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
 angular.module('life', []).controller('testCtrl', function ($scope, $timeout) {
-    var s = 20;
-    var maxCritterCount = 50;
-    ;
+    var s = 15;
+    var maxCritterCount = 15;
+    var moveChance = 0.10;
 
     var tile = (c, cl, x, y) => ({
         char: c, col: cl, x: x, y: y,
     });
     var critter = (c, cl, x, y)=> $.extend(tile(c, cl, x, y), {
         move: function () {
+            var xfac = 0
+            var yfac = 0;
+            if(rand(1/moveChance) < 1) {
+                if (rand(1) > 0) {
+                    xfac = randNeg(rand(1));
+                } else {
+                    yfac = randNeg(rand(1));
+                }
+            }
             return {
                 'from': this,
-                'to': {x: clamp(0, this.x + randNeg(rand(1)), s - 1), y: clamp(0, this.y + randNeg(rand(1)), s - 1)}
+                'to': {x: clamp(0, this.x + xfac, s - 1), y: clamp(0, this.y + yfac, s - 1)}
             };
         }
     });
@@ -20,17 +29,16 @@ angular.module('life', []).controller('testCtrl', function ($scope, $timeout) {
         {char: 'o', col: 'blue'},
         {char: 'a', col: 'purple'}];
 
+    var objs = '';
+    var initEngine = ()=>{
+        objs = new Array(s).fill('').map(o => ({}));
 
-    var objs = new Array(s).fill('').map(o => ({}));
-
-    for (var i = 0; i < maxCritterCount; i++) {
-        var a = pickRandom(alpha);
-        var x = rand(s - 1);
-        var y = rand(s - 1);
-        objs[x][y] = critter(a.char, a.col, x, y);
-    }
-
-    var render = () => {
+        for (var i = 0; i < maxCritterCount; i++) {
+            var a = pickRandom(alpha);
+            var x = rand(s - 1);
+            var y = rand(s - 1);
+            objs[x][y] = critter(a.char, a.col, x, y);
+        }
         $scope.field = new Array(s).fill('').map(
             (o, x) => new Array(s).fill('').map(
                 (o, y) => {
@@ -38,6 +46,12 @@ angular.module('life', []).controller('testCtrl', function ($scope, $timeout) {
                 }
             )
         );
+    }
+
+
+
+    var render = () => {
+
     }
 
     var doMoves = () => {
@@ -54,10 +68,11 @@ angular.module('life', []).controller('testCtrl', function ($scope, $timeout) {
             ;
         });
         moves.forEach(o => {
-            console.log(o.to.x);
             if (!eqCoords(o.to, o.from) && !objs[o.to.x][o.to.y]) {
                 objs[o.to.x][o.to.y] = o.from;
                 objs[o.from.x][o.from.y] = null;
+                $scope.field[o.to.x][o.to.y] = o.from;
+                $scope.field[o.from.x][o.from.y] = emptyTile(o.from.x, o.from.y);
                 o.from.x = o.to.x;
                 o.from.y = o.to.y;
             }
@@ -68,9 +83,14 @@ angular.module('life', []).controller('testCtrl', function ($scope, $timeout) {
         doMoves();
 
         render();
-        $timeout(engineLoop, 500);
+        $timeout(engineLoop, 1000/60);
     }
+
+
+
+    initEngine();
     engineLoop();
+
 
 })
 
