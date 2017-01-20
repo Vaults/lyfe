0d345be2041f47272b3856124ca25cc6db3e28bc
Marcel Visser <marcel1337@hotmail.com>
2017-01-20 01:17:35 +0100

    More behavior

--- js/main.js
+++ js/main.js
@@ -2,14 +2,17 @@ angular.module('life', []).controller('testCtrl', function ($scope, $timeout) {
     var s = 50;
     var maxCritterCount = s;
     var moveChance = 0.10;
-    var seqChance = 0.00001;
     var FPS = 30;
     var noWalls = !true;
     var wallFactor = 0.6;
+    var seqChance = 0.00069;
 
-    var tile = (c, cl, x, y) => ({
-        char: c, col: cl, x: x, y: y,
-    });
+    var tile = (c, cl, x, y) => {
+        var color = {};
+        if(typeof cl == "string"){color.toString = () => cl;}
+        else{ color = {hue: cl, sat: 60, br: 20, toString : function(){return 'hsl('+this.hue+','+this.sat+'%,'+this.br+'%);'}}}
+        return {char: c, col: color, x:x, y:y};
+    };
     var critter = (c, cl, x, y, rot)=> $.extend(tile(c, cl, x, y), {
         rotation: rot,
         sequences: {
@@ -47,12 +50,18 @@ angular.module('life', []).controller('testCtrl', function ($scope, $timeout) {
                 return this.currSequence.phase == this.currSequence.maxPhase;
             },
             run: function() {
-                var ret = this[this.currSequence.sequence].f(this.currSequence.phase, this.currSequence.parameters);
-                this.incPhase();
-                if (this.isDone()) {
-                    this.endSequence();
+                try {
+                    var ret = this[this.currSequence.sequence].f(this.currSequence.phase, this.currSequence.parameters);
+                    this.incPhase();
+                    if (this.isDone()) {
+                        this.endSequence();
+                    }
+                    return ret;
+                }catch(e){
+                    console.log('SEQUENCE BROKEN, EXCEPTION THROWN');
+                    console.log('sequence',this.currSequence.sequence,'state', this.currSequence.state);
+                    throw e;
                 }
-                return ret;
             },
 
             moveBlock: {
@@ -67,7 +76,8 @@ angular.module('life', []).controller('testCtrl', function ($scope, $timeout) {
                         "6": {x: 0, y: -1},
                         "7": {x: 0, y: -1},
                     }
-                    return dirs[p];
+                    if(par){ return dirs[p]; }
+                    return {x: -dirs[p].x, y: -dirs[p].y};
                 }, maxPhase: 7
             },
             moveLine: {
@@ -108,29 +118,45 @@ angular.module('life', []).controller('testCtrl', function ($scope, $timeout) {
             for(var i = -1; i <= 1; i++){
                 for(var j = -1; j <= 1; j++){
                     if(objs[x + i] && objs[x+i][y+j] && objs[x+i][y+j].move && Math.abs(i) != Math.abs(j)){
-                        ret.push([i,j]);
+                        ret.push([i,j, objs[x+i][y+j]]);
                     }
                 }
             }
           return ret;
         },
+        isThreat: function(c) {
+            return Math.abs(this.col.hue - c.col.hue) + rand(30) > 130;
+        },
         move: function () {
             var mod = {x: 0, y: 0};
             var surr = this.senseSurroundings(this.x, this.y);
 
-            if(!this.sequences.inSequence()) {
-                this.sequences.startSequence('moveRandom');
-            }
             if(surr.length > 0){
                 //oh no run away
-                if(rand(10) < 2) {
-                    this.sequences.startSequence('moveLine', {x: -(surr[0][0]), y: -(surr[0][1])});
+                if(this.isThreat(surr[0][2])) {
+                    if(rand(10) > 5) {
+                        this.sequences.startSequence('moveLine', {x: -(surr[0][0]), y: -(surr[0][1])});
+                    }else{
+                        var d;
+                        if(surr[0][0] > this.x){
+                            d = 'e';
+                        }else if(surr[0][0] < this.x){
+                            d = 'w';
+                        }else if(surr[0][1] < this.y){
+                            d = 'n';
+                        }else{
+                            d = 's';
+                        }
+                        this.sequences.startSequence('moveZigzag', d);
+                    }
                 }
-            } else if(!this.sequences.inSequence()){
-                var r = rand(10000);
-                if(r < 5){
-                    this.sequences.startSequence('moveBlock');
-                }else if(r < 10) {
+            }
+            if(!this.sequences.inSequence()){
+                this.sequences.startSequence('moveRandom');
+                var r = rand(1/seqChance);
+                if(r < 2){
+                    this.sequences.startSequence('moveBlock', rand(10) > 5);
+                }else if(r < 4) {
                     this.sequences.startSequence('moveZigzag', pickRandom(['n','w','s','e']));
                 }
             }
@@ -147,7 +173,7 @@ angular.module('life', []).controller('testCtrl', function ($scope, $timeout) {
         }
     });
     var emptyTile = (x, y) => tile(' ', 'lightgreen', x, y);
-    var wallTile = (x,y) => tile(' ', 'green',x,y);
+    var wallTile = (x,y) => tile(' ', 'steelblue',x,y);
     var alpha = ['x', 'a', 'o', 'c', 'e', 'u', 'z', 'n', 'w', 'v', '#', '@', '€', '^', '-', '+', '='];
 
     var objs = '';
@@ -170,6 +196,16 @@ angular.module('life', []).controller('testCtrl', function ($scope, $timeout) {
             }
         }
 
+        /*var bfs = (x, y) => {
+            var S = [];
+            var Q = [[x,y]];
+            while(Q.length > 0){
+                var curr = Q.pop();
+                for()
+            }
+
+        }*/
+
         for(x in tempMap){
             for(y in tempMap[x]){
                 if(y){
@@ -189,7 +225,13 @@ angular.module('life', []).controller('testCtrl', function ($scope, $timeout) {
             var a = pickRandom(alpha);
             var x = rand(s - 1);
             var y = rand(s - 1);
-            objs[x][y] = critter(a, 'hsl('+rand(0,360)+', 60%, 30%)' , x, y, pickRandom([90,180,270]));
+
+            var ch = (a,b) => objs[clamp(0,a,s-1)][clamp(0,b,s-1)];
+            while(ch(x-1,y) || ch(x+1,y) || ch(x,y-1) || ch(x,y+1)){
+                x = rand(s - 1);
+                y = rand(s - 1);
+            }
+            objs[x][y] = critter(a, rand(0,360), x, y, pickRandom([90,180,270]));
         }
 
         $scope.field = new Array(s).fill('').map(
